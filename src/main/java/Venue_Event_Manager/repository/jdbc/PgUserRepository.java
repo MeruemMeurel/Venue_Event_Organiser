8db3bd806062c6829ea003f9ee74ef070ac89407
Manuel <93267191+MeruemMeurel@users.noreply.github.com>
2026-03-09 20:23:42 +0100

    Add methods to find users in database by id,username,email or phone
    Signed-off-by: Manuel <93267191+MeruemMeurel@users.noreply.github.com>

--- src/main/java/Venue_Event_Manager/repository/jdbc/PgUserRepository.java
+++ src/main/java/Venue_Event_Manager/repository/jdbc/PgUserRepository.java
@@ -14,6 +14,9 @@ import java.util.Optional;
 public class PgUserRepository implements UserRepository {
 
     private static final String SQL_FIND_BY_ID = "SELECT * FROM user WHERE id = ?";
+    private static final String SQL_FIND_BY_EMAIL = "SELECT * FROM user WHERE email = ?";
+    private static final String SQL_FIND_BY_USERNAME = "SELECT * FROM user WHERE username = ?";
+    private static final String SQL_FIND_BY_PHONE = "SELECT * FROM user WHERE phone = ?";
 
     /**
      * Lambda function to implement RowMapper interface
@@ -33,7 +36,7 @@ public class PgUserRepository implements UserRepository {
     @Override
     public Optional<User> findById(Connection conn, long userId) {
         //Try-with-resources Java automatically calls for methdo ps.close() at the end of try catch
-        try (PreparedStatement ps = conn.prepareStatement(SQL_FIND_BY_ID);){
+        try (PreparedStatement ps = conn.prepareStatement(SQL_FIND_BY_ID)){
 
             ps.setLong(1, userId);
 
@@ -51,17 +54,56 @@ public class PgUserRepository implements UserRepository {
 
     @Override
     public Optional<User> findByEmail(Connection conn, String email) {
-        return Optional.empty();
+        try (PreparedStatement ps = conn.prepareStatement(SQL_FIND_BY_EMAIL)) {
+
+            ps.setString(1, email);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (!rs.next()) return Optional.empty();
+                return Optional.of(user_mapper.mapRow(rs));
+            }
+
+
+        }catch(SQLException e){
+            throw new DaoException("Error while trying to find user with email " + email, e);
+        }
+
     }
 
     @Override
     public Optional<User> findByUsername(Connection conn, String username) {
-        return Optional.empty();
+
+        try(PreparedStatement ps = conn.prepareStatement(SQL_FIND_BY_USERNAME)){
+
+            ps.setString(1,username);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if(!rs.next()) return Optional.empty();
+                return Optional.of(user_mapper.mapRow(rs));
+
+            }
+
+        }catch(SQLException e){
+            throw new DaoException("Error while trying to find user with username " + username, e);
+        }
+
     }
 
     @Override
     public Optional<User> findByPhone(Connection conn, String phone) {
-        return Optional.empty();
+        try(PreparedStatement ps = conn.prepareStatement(SQL_FIND_BY_PHONE)){
+
+            ps.setString(1,phone);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if(!rs.next()) return Optional.empty();
+                return Optional.of(user_mapper.mapRow(rs));
+
+            }
+
+        }catch(SQLException e){
+            throw new DaoException("Error while trying to find user with phone " + phone, e);
+        }
     }
 
     @Override
