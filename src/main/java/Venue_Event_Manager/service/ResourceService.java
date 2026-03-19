9c8532fedca0274743e9cd8ea04d11a8de34ba27
Manuel <93267191+meruemmeurel@users.noreply.github.com>
2026-03-19 23:29:28 +0100

    Add get methods to ResourceService
    Signed-off-by: Manuel <93267191+MeruemMeurel@users.noreply.github.com>

--- src/main/java/Venue_Event_Manager/service/ResourceService.java
+++ src/main/java/Venue_Event_Manager/service/ResourceService.java
@@ -1,9 +1,17 @@
 package Venue_Event_Manager.service;
 
 import Venue_Event_Manager.config.TransactionManager;
+import Venue_Event_Manager.domain.model.resource.Equipment;
+import Venue_Event_Manager.domain.model.resource.Resource;
+import Venue_Event_Manager.domain.model.resource.Service;
+import Venue_Event_Manager.domain.model.resource.Space;
+import Venue_Event_Manager.exception.NotFoundException;
 import Venue_Event_Manager.repository.EquipmentRepository;
 import Venue_Event_Manager.repository.ServiceRepository;
 import Venue_Event_Manager.repository.SpaceRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ResourceService {
 
@@ -19,6 +27,88 @@ public class ResourceService {
         this.serviceRepository = serviceRepository;
     }
 
-    public
+    public List<Resource> getAllResources(){
+        ArrayList<Resource> resources = new ArrayList<>();
+
+        resources.addAll(getAllSpaces());
+        resources.addAll(getAllServices());
+        resources.addAll(getAllEquipments());
+
+        return resources;
+    }
+
+    public List<Space> getAllSpaces(){
+        return transactionManager.inReadOnly(conn ->
+                spaceRepository.findAll(conn));
+    }
+
+    public List<Service> getAllServices(){
+        return transactionManager.inReadOnly(conn ->
+                serviceRepository.findAll(conn));
+    }
+
+    public List<Equipment> getAllEquipments(){
+        return  transactionManager.inReadOnly(conn ->
+                equipmentRepository.findAll(conn));
+    }
+
+    public List<Resource> searchResourceByName(String name){
+        ArrayList<Resource> resources = new ArrayList<>();
+        resources.addAll(searchSpaceByName(name));
+        resources.addAll(searchServiceByName(name));
+        resources.addAll(searchEquipmentByName(name));
+        return resources;
+    }
+
+    public List<Space> searchSpaceByName(String name){
+        return transactionManager.inReadOnly(conn->
+                spaceRepository.searchByName(conn, name));
+    }
+
+    public List<Service> searchServiceByName(String name){
+        return transactionManager.inReadOnly(conn->
+                serviceRepository.searchByName(conn, name));
+    }
+
+    public List<Equipment> searchEquipmentByName(String name){
+        return transactionManager.inReadOnly(
+                conn->equipmentRepository.searchByName(conn, name)
+        );
+    }
+
+    public Space getSpaceById(long id){
+        return transactionManager.inReadOnly(conn ->
+                spaceRepository.findById(conn,id)
+                        .orElseThrow(() -> new NotFoundException("No space found with id "+id)));
+    }
+
+    public Service getServiceById(long id){
+        return transactionManager.inReadOnly(conn ->
+                serviceRepository.findById(conn,id)
+                        .orElseThrow(() -> new NotFoundException("No service found with id "+id)));
+    }
+
+    public Equipment getEquipmentById(long id){
+        return transactionManager.inReadOnly(conn ->
+                equipmentRepository.findById(conn,id)
+                        .orElseThrow(() -> new NotFoundException("No equipment found with id "+id)));
+    }
+
+    public List<Resource> getResourcesByVenue(long venueId){
+        ArrayList<Resource> resources = new ArrayList<>();
+        resources.addAll(getSpaceByVenue(venueId));
+        resources.addAll(getEquipmentByVenue(venueId));
+        return resources;
+    }
+
+    public List<Space> getSpaceByVenue(long venueId){
+        return transactionManager.inReadOnly(conn->
+                spaceRepository.findAllByVenueId(conn,venueId));
+    }
+
+    public List<Equipment> getEquipmentByVenue(long venueId){
+        return transactionManager.inReadOnly(conn->
+                equipmentRepository.findAllByVenueId(conn,venueId));
+    }
 
 }
