9d85ecc41c308e90fbc5001888d878f4b8d33656
marcocarri <carricatomarco@gmail.com>
2026-05-19 17:04:01 +0200

    begin implementation of TestDataFactory class, delete gitkeep
    Signed-off-by: marcocarri <carricatomarco@gmail.com>

---
+++ src/main/java/Venue_Event_Manager/util/TestDataFactory.java
@@ -0,0 +1,9 @@
+package Venue_Event_Manager.util;
+
+/**
+ * A tool for creating mock domain models for testing purposes.
+ * Eliminates code repetition in unit and integration tests.
+ */
+public class TestDataFactory {
+
+}
