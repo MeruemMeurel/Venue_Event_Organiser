b8a9d400c5b0278da500d6f3c048469a974a8b44
marcocarri <carricatomarco@gmail.com>
2026-05-19 17:11:43 +0200

    implementation of the User section in TestDataFactory class
    Signed-off-by: marcocarri <carricatomarco@gmail.com>

--- src/main/java/Venue_Event_Manager/util/TestDataFactory.java
+++ src/main/java/Venue_Event_Manager/util/TestDataFactory.java
@@ -1,9 +1,48 @@
 package Venue_Event_Manager.util;
 
+import Venue_Event_Manager.domain.model.user.User;
+import Venue_Event_Manager.domain.model.user.AccountStatus;
+
+import java.time.LocalDate;
+
 /**
  * A tool for creating mock domain models for testing purposes.
  * Eliminates code repetition in unit and integration tests.
  */
 public class TestDataFactory {
+
+    /**
+     * USER
+     */
+    /**
+     * Creates a standard, active, non-admin user with default mock data.
+     */
+    public static User createDefaultUser(String username) {
+        return new User(
+                0, // default ID for unsaved objects
+                username,
+                "Mario",
+                "Rossi",
+                LocalDate.of(2000, 1, 1),
+                username + "@example.com",
+                "+391234567890",
+                false,
+                AccountStatus.ACTIVE
+        );
+    }
+
+    /**
+     * Creates an admin user.
+     */
+    public static User createAdminUser(String username) {
+        return createDefaultUser(username).withIsAdmin(true);
+    }
+
+    /**
+     * Creates a banned user.
+     */
+    public static User createBannedUser(String username) {
+        return createDefaultUser(username).withAccountStatus(AccountStatus.BANNED);
+    }
 
 }
